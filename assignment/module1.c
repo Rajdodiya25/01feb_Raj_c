85bab2a4375b90030c4663bf7ff1d8d45a1cbc4e
Raj Dodiya <123078330+Rajdodiya25@users.noreply.github.com>
2023-02-21 11:16:50 +0530

    Add files via upload

---
+++ assignment/module1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    printf("name: raj nathabhai dodiya");
+    printf("\n add mangrol vadi vistar");
+    printf("\n birth: 01-01-2004");
+    printf("\n age: 19");
+}
