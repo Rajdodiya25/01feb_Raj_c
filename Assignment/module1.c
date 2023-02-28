3a994e03e40a1c3e0017277f05f089c291cfb7a7
Raj Dodiya <123078330+Rajdodiya25@users.noreply.github.com>
2023-02-28 16:48:31 +0530

    Delete module1.c

--- Assignment/module1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("name: raj nathabhai dodiya");
-    printf("\n add mangrol vadi vistar");
-    printf("\n birth: 01-01-2004");
-    printf("\n age: 19");
-}
