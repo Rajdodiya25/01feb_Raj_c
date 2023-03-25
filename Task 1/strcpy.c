9cb656996507def59817b6bb0aebe78c84eee833
Raj Dodiya <123078330+Rajdodiya25@users.noreply.github.com>
2023-03-25 12:28:31 +0530

    Create strcpy.c

---
+++ Task 1/strcpy.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    char string1[20],string2[20];
+    printf("\n Enter the first string : ");
+    scanf("%s",&string1);
+    strcpy(string2 , string1);
+    printf("\n Your name is :%s",string2);
+}
