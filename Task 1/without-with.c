5b275b3ea07d8741fb5955759d17a937d1372f8a
Raj Dodiya <123078330+Rajdodiya25@users.noreply.github.com>
2023-03-28 21:30:43 +0530

    Create without-with.c

---
+++ Task 1/without-with.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void data(int no)
+{
+    printf("\nvalue of no is : %d",no);
+}
+void main()
+{
+    int a;
+    printf("\nEnter the value of a :");
+    scanf("%d",&a);
+    data(a);
+}
